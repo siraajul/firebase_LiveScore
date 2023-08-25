6aa42cb5b8770d03428fd3392405460885f5f214
siraajul <shirajulislamparvez@gmail.com>
2023-08-25 20:12:20 +0600

    Project Update

--- lib/match_two.dart
+++ lib/match_two.dart
@@ -1,8 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class MatchOne extends StatelessWidget {
-  const MatchOne({super.key});
+class MatchTwo extends StatelessWidget {
+  const MatchTwo({super.key});
 
   @override
   Widget build(BuildContext context) {
